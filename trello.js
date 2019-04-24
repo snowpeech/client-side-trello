7382baf6b15635d844d62b8a972673f66d2cec9c
snowpeech <snowpeech@gmail.com>
2019-04-23 21:05:54 -0500

    can add a swim lane

--- trello.js
+++ trello.js
@@ -1,4 +1,21 @@
+var pool =[];
+var display = document.getElementById("#pool");
+var laneID = 1;
+var cardID = 0;
+
 function addLane(){
-    var laneName = document.getElementById("laneTitle");
-    alert(laneName.value);
+    var laneName = document.getElementById("laneTitle").value;
+
+    // var lane = [''];
+    // pool.push(lane)
+    // alert(laneName.value);
+    console.log(laneName);
+
+    var div = document.createElement("DIV"); 
+    div.setAttribute("id", "swimlane" + laneID);   
+    div.setAttribute("class", "lane");
+    div.innerHTML=`<h5>${laneName}</h5>`;
+
+    document.querySelector("#pool").appendChild(div);
+    document.getElementById("laneTitle").value="";
 }
