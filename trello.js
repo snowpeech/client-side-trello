24f91e25ebcde749ee8329553f5891f448cce106
snowpeech <snowpeech@gmail.com>
2019-04-30 16:48:32 -0500

    styling

--- trello.js
+++ trello.js
@@ -1,3 +1,4 @@
+"use strict";
 var pool =[]; // unnecessary?
 var display = document.getElementById("#pool");
 var laneID = 0;
@@ -18,17 +19,20 @@ function enterKey() {
 function addLane(){
     
     var laneName = document.getElementById("laneTitle").value;
-
-    laneID ++;
+    if (laneName){
+        
+        laneID ++;
+        
+        var lane = document.createElement('div');
+        lane.setAttribute('class', 'list');
+        lane.setAttribute("draggable", "true");
+        lane.innerHTML = `<div class ="listTitle">${laneName}</div>`+ `<button onclick="deleteCard(this, ${laneID})"><i class="far fa-trash-alt"></i></button>`+ `<button id="" onclick="moveLeft()" ><i class="fas fa-caret-left"></i></button>
+        <button id="" onclick="moveRight()" ><i class="fas fa-caret-right"></i></button><div class = "class" id="card-container-${laneID}"><input type = "text" id="lanecard${laneID}" name="cardtxt" placeholder="Add a card..."><button type="button" onclick="addCard(${laneID})">+</button></div>`
     
-    var lane = document.createElement('div');
-    lane.setAttribute('class', 'list');
-    lane.innerHTML = `<div class ="listTitle">${laneName}</div>`+ `<button onclick="deleteCard(this, ${laneID})">x</button>`+ `<button id="" onclick="moveLeft()" ><i class="fas fa-caret-left"></i></button>
-    <button id="" onclick="moveRight()" ><i class="fas fa-caret-right"></i></button><div class = "card" id="card-container-${laneID}"><input type = "text" id="lanecard${laneID}" name="cardtxt" placeholder="Add a card"><button type="button" onclick="addCard(${laneID})">+</button></div>`
-
-    document.getElementById("laneTitle").value="";
-    var list_container = document.querySelector("#pool");
-    list_container.prepend(lane);  //adds to top of list
+        document.getElementById("laneTitle").value="";
+        var list_container = document.querySelector("#pool");
+        list_container.prepend(lane);  //adds to top of list
+        }
     }
 
 
@@ -51,11 +55,10 @@ function addCard(laneID) {
     var div = document.createElement("div");
     div.setAttribute("id", "card" + cardID); 
     div.setAttribute("class", "card");
-    
-    //div.innerHTML = `<button onclick="deleteCard(this, ${cardID})">x</button>`;
+    div.setAttribute("draggable", "true");  
 
     var title = document.createElement("P");
-    title.innerHTML = txtTitle + `<button onclick="deleteCard(this, ${cardID})">x</button> <button onclick="moveUp()" ><i class="fas fa-caret-up"></i></button><button onclick="moveDown()" ><i class="fas fa-caret-down"></i></button>`;
+    title.innerHTML = txtTitle + `<button onclick="editTxt(this)"><i class="fas fa-pen"></i></button> <button onclick="deleteCard(this, ${cardID})"><i class="far fa-trash-alt"></i></button> <button onclick="moveUp()" ><i class="fas fa-caret-up"></i></button><button onclick="moveDown()" ><i class="fas fa-caret-down"></i></button>`;
 
     div.appendChild(title);
 
@@ -70,6 +73,11 @@ function addCard(laneID) {
 function deleteCard(item, itemID){
     item.parentElement.style.display = "none";
     item.parentElement.style.border = "0px solid black0"; //why doesn't this make borders disappear?
+}
+
+function editTxt(item){
+    console.log("edit text")
+    console.log(item.parentElement)
 }
 
 function moveLeft(){
