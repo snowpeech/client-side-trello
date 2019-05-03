f2f0e2c5cfa9b7b2a798fbd17bda808a937c1850
snowpeech <snowpeech@gmail.com>
2019-05-02 22:03:04 -0500

    moving things start

--- trello.js
+++ trello.js
@@ -26,7 +26,8 @@ function addLane(){
         var lane = document.createElement('div');
         lane.setAttribute('class', 'list');
         lane.setAttribute("draggable", "true");
-        lane.innerHTML = `<div class ="listTitle">${laneName}</div>`+ `<button onclick="deleteLane(this, ${laneID})"><i class="far fa-trash-alt"></i></button>`+ `<button id="" onclick="moveLeft()" ><i class="fas fa-caret-left"></i></button>
+        lane.setAttribute("laneID",laneID);
+        lane.innerHTML = `<div class ="listTitle">${laneName}</div>`+ `<button onclick="deleteLane(this, ${laneID})"><i class="far fa-trash-alt"></i></button>`+ `<button id="" onclick="moveLeft(event)" ><i class="fas fa-caret-left"></i></button>
         <button id="" onclick="moveRight()" ><i class="fas fa-caret-right"></i></button><div class = "class" id="card-container-${laneID}"><input type = "text" id="lanecard${laneID}" name="cardtxt" placeholder="Add a card..."><button class="plusBtn" type="button" onclick="addCard(${laneID})"><i class="fas fa-plus"></i></button></div>`
     
         document.getElementById("laneTitle").value="";
@@ -49,7 +50,7 @@ function addCard(laneID) {
 
     var title = document.createElement("P");
     title.id = `card-${cardID}-text`
-    title.innerHTML = `<div id="title-${cardID}">${txtTitle}</div>` + `<button onclick="editTxt(event, ${cardID})"><i class="fas fa-pen"></i></button> <button onclick="deleteCard(this, ${cardID})"><i class="far fa-trash-alt"></i></button> <button onclick="moveUp()" ><i class="fas fa-caret-up"></i></button><button onclick="moveDown()" ><i class="fas fa-caret-down"></i></button>`;
+    title.innerHTML = `<div id="title-${cardID}">${txtTitle}</div>` + `<button onclick="editTxt(event, ${cardID})"><i class="fas fa-pen"></i></button> <button onclick="deleteCard(this, ${cardID})"><i class="far fa-trash-alt"></i></button><button id="" onclick="moveLeft(event,this)" ><i class="fas fa-caret-left"></i></button> <button onclick="moveUp()" ><i class="fas fa-caret-up"></i></button><button onclick="moveDown()" ><i class="fas fa-caret-down"></i></button><button id="" onclick="moveRight()" ><i class="fas fa-caret-right"></i></button>`;        
 
     div.appendChild(title);
 
@@ -78,9 +79,9 @@ function deleteLane(item, itemID){
 
 function editTxt(e,cardID){
     let editCheck = document.getElementById(`editting-${cardID}`);
-    console.log(editCheck)
+    
     if (editCheck){
-        console.log("already exists")
+        //do nothing - edit field already exists
     }
     else {
         console.log("proceed to create edit field")
@@ -106,29 +107,28 @@ function editTxt(e,cardID){
  }
 
  function grabText(t,cardID){
-    console.log("parentel",t.parentElement)
-    console.log(cardID);
-    let newWords = document.getElementById(`editting-${cardID}`).value;//need to update here with cardID
-    // document.getElementById("editting").value = "";
-    console.log(newWords);
-    // let target = t.parentElement.children[0].innerText;
+    let newWords = document.getElementById(`editting-${cardID}`).value;
+    
     let target = document.querySelector(`#title-${cardID}`);
-    console.dir(target);
+
     target.innerText = newWords;
 
     let del1 = document.getElementById(`editting-${cardID}`);
     let del2 = document.getElementById(`edittingbtn-${cardID}`);
-    // console.log("del1:", del1)
-    // console.log("del2:",del2)
-    // del1.style.display = "none";
-    // del2.style.display = "none";
+    
     del1.parentNode.removeChild(del1);
     del2.parentNode.removeChild(del2);
 
 }
 
-function moveLeft(){
+function moveCard(){
+    let laneID = this;
+}
+
+function moveLeft(e,t){
     console.log("move left placeholder")
+    console.dir(e)
+    console.dir(t)
 }
 
 function moveRight(){
